5fcfa17ce118bd47eea558ee59c5e3b23a4475fd
Nessrine54 <78733355+Nessrine54@users.noreply.github.com>
2021-02-10 21:34:48 +0100

    Notre Page

--- meteo.js
+++ meteo.js
@@ -1,16 +1,17 @@
-<script>
-    function showCity() {
-        
-        let nomVilleSelectionnee = document.getElementById("Toto").value;
-        let cities = document.getElementsByClassName("city");
-        let l=cities.length;
-        for (i=0; i<l; i++) {
-if (cities[i].id ==  nomVilleSelectionnee) {
-  cities[i].style.display= "";
-  
-} else {
-    cities[i].style.display= "none";
+
+function showCity() {
+			
+            let nomVilleSelectionnee = document.getElementById("Toto").value;
+            let cities = document.getElementsByClassName("city");
+            let l=cities.length;
+            for (i=0; i<l; i++) {
+    if (cities[i].id ==  nomVilleSelectionnee) {
+      cities[i].style.display= "";
+	  
+    } else {
+        cities[i].style.display= "none";
+    }
+  }
 }
-}
-}
-</script>
+   
+
